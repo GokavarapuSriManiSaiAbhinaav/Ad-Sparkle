0f06cae45f41065a49ad5438aaf827e0504d3fc0
GokavarapuSriManiSaiAbhinaav <2300031133@kluniversity.in>
2026-02-26 00:28:18 +0530

    login issue solved

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+let supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+if (supabaseUrl && !supabaseUrl.startsWith('http')) {
+    supabaseUrl = `https://${supabaseUrl}`;
+}
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
